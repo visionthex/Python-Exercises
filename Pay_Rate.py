55158550846cd1a64a76a30f17c723bd91bc8c1c
visionthex <sandseclos@gmail.com>
2026-05-25 19:21:23 -0400

    Changing the rate_pay to a float instead of in intager.

--- Pay_Rate.py
+++ Pay_Rate.py
@@ -3,8 +3,8 @@ rate = input('Enter Rate: ')
 
 try:
     hours_pay = int(hours)
-    rate_pay = int(rate)
-    
+    rate_pay = float(rate)
+
     if int(hours) > 40:
         normal_pay = 40 * rate_pay
         overtime_pay = (hours_pay - 40) * (rate_pay * 1.5)
